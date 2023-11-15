6d381d1093b97aa7ca2cb14646c85c844ba3659c
Parikshith <apple@Parikshiths-MBP.Dlink>
2023-11-15 21:50:41 +0530

    first template commit

---
+++ src/UserDirectory.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserDirectory = () => {
+
+  return (
+    <div>
+      This is UserDirectory
+    </div>
+  );
+};
+
+export default UserDirectory;
---
+++ src/UserProfile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserProfile = () => {
+  
+  return (
+    <div>
+      This is user Profile
+    </div>
+  );
+};
+
+export default UserProfile;
